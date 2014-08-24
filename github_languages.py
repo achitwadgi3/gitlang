6fed4ba2b763e27a6a1fa0a19e989a41c2d6290e
Ashutosh Chitwadgi <achitwadgi3@gmail.com>
2014-08-24 12:27:13 -0700

    add language query function

--- github_languages.py
+++ github_languages.py
@@ -16,10 +16,25 @@ def get_repos(user):
 	logging.debug("Retrieving user {user} repos".format(user=user))
 	return response.json()
 
+def get_lang_dict(repos):
+	""" 
+	Return a list of dicts containing languages used in each repo
+	"""
+	lang_dict =[]
+	for repo in repos:
+		url = "https://api.github.com/repos/{owner}/{reposit}/languages".format(owner=repo["owner"]["login"], reposit = repo["name"] )
+		logging.debug("IN GET_LANG_URL: {}".format(url))
+		print url
+		response = requests.get(url, auth=(USERNAME,PASSWORD))
+		lang_dict.append(response.json())
+	return lang_dict
+
 def main():
 	"""Main"""
 	repos=get_repos(sys.argv[1])
 	logging.debug("In main: get repo for argv[1]")
-	print repos
+	lang_dicts= get_lang_dict(repos)
+	print lang_dicts
+	#print repos[1]
 
 if __name__ == "__main__": main()
